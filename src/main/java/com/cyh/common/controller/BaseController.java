3e0e06dc0c7410bff308378dbd042f3e0d294cdb
cyh3101 <376957356@qq.com>
2017-07-08 15:02:06 +0800

    2017-07-08

---
+++ src/main/java/com/cyh/common/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.cyh.common.controller;
+
+/**
+ * Created by cai on 2017/7/8.
+ */
+public class BaseController {
+}
---
+++ src/main/java/com/cyh/user/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.cyh.user.controller;
+
+/**
+ * Created by cai on 2017/7/8.
+ */
+public class HomeController {
+}
