117fe69391d0146860f566ac1ac63c199a78e055
cyh3101 <376957356@qq.com>
2017-08-03 10:21:11 +0800

    1.增加logger工具类

--- src/main/java/com/cyh/common/utils/LoggerUtils.java
+++ src/main/java/com/cyh/common/utils/LoggerUtils.java
@@ -1,9 +1,93 @@
 package com.cyh.common.utils;
 
+import org.apache.log4j.Logger;
+
 /**
  * 日志工具类
  * Created by cai on 2017/8/2.
  */
 public class LoggerUtils {
+    public static boolean isDebug = Logger.getLogger(LoggerUtils.class).isDebugEnabled();
 
+    /**
+     * debug
+     * @param clazz
+     * @param message
+     */
+    public static void debug(Class<? extends Object> clazz, String message){
+        if(!isDebug)return;
+        Logger logger = Logger.getLogger(clazz);
+        logger.debug(message);
+    }
+
+    /**
+     * error
+     * @param clazz
+     * @param message
+     * @param e
+     */
+    public static void error(Class<? extends Object> clazz, String message, Exception e){
+        if(!isDebug)return;
+        Logger logger = Logger.getLogger(clazz);
+        if(null == e){
+            logger.error(message);
+            return;
+        }
+        logger.error(message, e);
+    }
+
+    /**
+     * error重载
+     * @param clazz
+     * @param message
+     */
+    public static void error(Class<? extends Object> clazz, String message){
+        error(clazz, message, null);
+    }
+
+    /**
+     * 格式化输出debug
+     * @param clazz
+     * @param fmtString
+     * @param value
+     */
+    public static void fmtDebug(Class<? extends Object> clazz, String fmtString,String...value){
+        if(!isDebug)return;
+        if(StringUtils.isBlank(fmtString)){
+            return;
+        }
+        if(null != value && value.length != 0){
+            fmtString = String.format(fmtString, value);
+        }
+        debug(clazz, fmtString);
+    }
+
+    /**
+     * 格式化输出error
+     * @param clazz
+     * @param e
+     * @param fmtString
+     * @param value
+     */
+    public static void fmtError(Class<? extends Object> clazz, Exception e, String fmtString, String...value){
+        if(!isDebug)return;
+        if(StringUtils.isBlank(fmtString)){
+            return;
+        }
+        if(null != value && value.length !=0){
+            fmtString = String.format(fmtString, value);
+        }
+        error(clazz, fmtString, e);
+    }
+
+    public static void fmtError(Class<? extends Object> clazz, String fmtString, String...value){
+        if(!isDebug)return;
+        if(StringUtils.isBlank(fmtString)){
+            return;
+        }
+        if(null != value && value.length != 0){
+            fmtString = String.format(fmtString, value);
+        }
+        error(clazz, fmtString);
+    }
 }
