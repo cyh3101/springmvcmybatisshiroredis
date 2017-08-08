681ee1ff2404aff18befaf030b01df09c549d8f9
cyh3101 <376957356@qq.com>
2017-08-08 23:12:09 +0800

    1.增加UtilPath类

--- src/main/java/com/cyh/common/utils/UtilPath.java
+++ src/main/java/com/cyh/common/utils/UtilPath.java
@@ -4,6 +4,10 @@ package com.cyh.common.utils;
  * Created by cyh on 2017/8/8.
  */
 public class UtilPath {
+    /**
+     * 获得classs目录
+     * @return
+     */
     public static String getClassPath(){
         String sysName = System.getProperty("os.name");
 
@@ -13,5 +17,26 @@ public class UtilPath {
         }else {
             return UtilPath.class.getResource("/").getFile().toString();
         }
+    }
+
+    /**
+     * 获取当前对象的路径
+     * @param object
+     * @return
+     */
+    public static String getObjectPath(Object object){
+        return object.getClass().getResource(".").getFile().toString();
+    }
+
+    /**
+     * 获得项目路径
+     * @return
+     */
+    public static String getProjectPath(){
+        return System.getProperty("user.dir");
+    }
+
+    public static String getWEB_INF(){
+        return getClassPath().replace("classes/","");
     }
 }
